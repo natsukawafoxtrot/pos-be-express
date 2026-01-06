a16649f77b6312db82136068670936c06bfd97da
bstka <bismantaka20@hotmail.com>
2026-01-06 13:20:10 +0700

    update: adding role model

---
+++ src/databases/models/role.ts
@@ -0,0 +1,14 @@
+import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+
+@Entity()
+export class Role {
+
+   @PrimaryKey()
+   id!: number;
+
+   @Property()
+   name!: string;
+
+   @Property()
+   display_name!: string;
+}
