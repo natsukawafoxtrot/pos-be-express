db74177a8dbd0f40ca992f209ddd705c5662df7d
bstka <bismantaka20@hotmail.com>
2026-01-06 13:54:57 +0700

    update: adding category model

---
+++ src/databases/models/category.ts
@@ -0,0 +1,14 @@
+import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+
+@Entity()
+export class Category {
+
+   @PrimaryKey()
+   id!: number;
+
+   @Property()
+   name!: string;
+
+   @Property()
+   description!: string;
+}
