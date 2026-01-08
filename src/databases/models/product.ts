64b9fbae7df66b7e1012d3b67c6340f031f17a8d
bstka <bismantaka20@hotmail.com>
2026-01-08 11:47:13 +0700

    change: adding few colums

--- src/databases/models/product.ts
+++ src/databases/models/product.ts
@@ -1,17 +1,36 @@
 import { Entity, ManyToOne, PrimaryKey, Property } from "@mikro-orm/core";
 import { Category } from "./category.js";
+import { Unit } from "./unit.js";
 
 @Entity()
 export class Product {
-  @PrimaryKey()
+  @PrimaryKey({ type: "uuid" })
   id!: number;
+
+  @ManyToOne(() => Category)
+  category!: Category;
+
+  @ManyToOne(() => Unit)
+  unit!: Unit;
+
+  @Property({ unique: true, length: 50 })
+  sku!: string;
 
   @Property()
   name!: string;
 
-  @Property()
-  description!: string;
+  @Property({ length: 100, nullable: true })
+  barcode?: string;
 
-  @ManyToOne({ entity: () => Category })
-  category!: Category;
+  @Property({ columnType: "numeric(12,2)", default: "0.00" })
+  currentPrice: string = "0.00";
+
+  @Property({ columnType: "numeric(12,2)", default: "0.00" })
+  currentCostPrice: string = "0.00";
+
+  @Property({ default: true })
+  isActive: boolean = true;
+
+  @Property({ defaultRaw: "now()" })
+  createdAt: Date = new Date();
 }
